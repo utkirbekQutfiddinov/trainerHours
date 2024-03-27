e1ea48370e0b456c346bdb94b9ff521bc59ef71e
utkirbek <utkirbek@mail.ru>
2024-03-27 06:00:47 +0500

    first commit

---
+++ src/main/java/com/epam/trainerhours/TrainerHoursApplication.java
@@ -0,0 +1,13 @@
+package com.epam.trainerhours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerHoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainerHoursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/trainerhours/TrainerHoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.trainerhours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerHoursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
